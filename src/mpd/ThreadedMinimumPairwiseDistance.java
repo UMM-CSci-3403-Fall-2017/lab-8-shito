f8e75a3681c5d2debcc610f1aaeeaa0266475e16
Rocherno De Jongh <dejon076@spacewhale.morris.umn.edu>
2017-12-04 02:06:26 -0600

    Starting by adding the inner classes in ThreadedMinimumPairWise, created method updateGlobalResult

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,10 +1,61 @@
 package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+	public final int GLOBALRESULT = Integer.MAX_VALUE;
+	
+	@Override
+	public int minimumPairwiseDistance(int[] values) {
+		
+		Thread[] threads = new Thread[values.length];
+		throw new UnsupportedOperationException();
 
-    @Override
-    public int minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
-    }
+	}
+	private int updateGlobalResult(int localResult){
+		if(localResult < GLOBALRESULT){
+			localResult = GLOBALRESULT;
+		}
+		
+		return localResult;
+	}
+	
+	private class lowerLeft implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	private class bottomRight implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	private class topRight implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	private class center implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 
 }
