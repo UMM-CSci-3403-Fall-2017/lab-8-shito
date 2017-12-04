88b1dc6ce5f0c98130c3e219d8c37ff5d0276ba8
Rocherno De Jongh <dejon076@spacewhale.morris.umn.edu>
2017-12-04 05:10:16 -0600

    Added code to the inner classes, trying to figure out the bounds, also needs to figure out how to use the values length in the inner classes

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -2,60 +2,108 @@ package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
 	public final int GLOBALRESULT = Integer.MAX_VALUE;
-	
+
 	@Override
 	public int minimumPairwiseDistance(int[] values) {
-		
-		Thread[] threads = new Thread[values.length];
+
+		lowerLeft thread1 = new lowerLeft();
+		bottomRight thread2 = new bottomRight();
+		topRight thread3 = new topRight();
+		center thread4 = new center();
+
 		throw new UnsupportedOperationException();
 
 	}
-	private int updateGlobalResult(int localResult){
-		if(localResult < GLOBALRESULT){
+
+	public synchronized void updateGlobalResult(int localResult) {
+		if (localResult < GLOBALRESULT) {
 			localResult = GLOBALRESULT;
 		}
-		
-		return localResult;
+
 	}
-	
-	private class lowerLeft implements Runnable{
+
+	private class lowerLeft implements Runnable {
 
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			
+			int result = Integer.MAX_VALUE;
+			for (int i = 0; i < values.length / 2; ++i) {
+				for (int j = 0; j < i; ++j) {
+					// Gives us all the pairs (i, j) where 0 ≤ j < i <
+					// values.length
+					int diff = Math.abs(values[i] - values[j]);
+					if (diff < result) {
+						result = diff;
+					}
+				}
+			}
+			return result;
 		}
-		
+
 	}
-	
-	private class bottomRight implements Runnable{
+
+	private class bottomRight implements Runnable {
 
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			
+			int result = Integer.MAX_VALUE;
+			for (int i = 0; i < values.length / 2; ++i) {
+				for (int j = 0; j < i; ++j) {
+					// Gives us all the pairs (i, j) where 0 ≤ j < i <
+					// values.length
+					int diff = Math.abs(values[i] - values[j]);
+					if (diff < result) {
+						result = diff;
+					}
+				}
+			}
+			return result;
 		}
-		
+
 	}
-	
-	private class topRight implements Runnable{
+
+	private class topRight implements Runnable {
 
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			
+			int result = Integer.MAX_VALUE;
+			for (int i = 0; i < values.length / 2; ++i) {
+				for (int j = 0; j < i; ++j) {
+					// Gives us all the pairs (i, j) where 0 ≤ j < i <
+					// values.length
+					int diff = Math.abs(values[i] - values[j]);
+					if (diff < result) {
+						result = diff;
+					}
+				}
+			}
+			return result;
 		}
-		
+
 	}
-	
-	private class center implements Runnable{
+
+	private class center implements Runnable {
 
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			
+			int result = Integer.MAX_VALUE;
+			for (int i = 0; i < values.length / 2; ++i) {
+				for (int j = 0; j < i; ++j) {
+					// Gives us all the pairs (i, j) where 0 ≤ j < i <
+					// values.length
+					int diff = Math.abs(values[i] - values[j]);
+					if (diff < result) {
+						result = diff;
+					}
+				}
+			}
+			return result;
 		}
-		
+
 	}
 
 }
